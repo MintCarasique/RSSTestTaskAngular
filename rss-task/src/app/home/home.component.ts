b8eb887e68f671368916f4e75d83209a4d140f9f
Artem Slizhevskiy <mykew98@gmail.com>
2018-06-17 17:33:10 +0300

    Fixes for pagination. Now it's fully working. Now home page uses server side pagination

--- rss-task/src/app/home/home.component.ts
+++ rss-task/src/app/home/home.component.ts
@@ -4,25 +4,32 @@ import { News } from '../shared/news.model';
 import { NewsPage } from '../shared/news-page.model';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
     newsList: News[];
     newsPage: NewsPage;
-    currentPage:number;
-  constructor(private newsService: NewsService) { }
+    currentPage: number;
+    collectionSize: number;
+    constructor(private newsService: NewsService) { }
 
-  ngOnInit() {
-    //this.newsService.getNews().subscribe(data => {this.newsList = data})
-    this.newsService.getNewsPage(1).subscribe(data => {this.newsPage = data})
-    this.currentPage = 1;
-  }
+    ngOnInit() {
+        //this.newsService.getNews().subscribe(data => {this.newsList = data})
+        this.newsService.getNewsPage(1).subscribe(data => {
+            this.newsPage = data;
+            this.collectionSize = data.CollectionSize;
+        })
+        this.currentPage = 1;
+    }
 
-  getNewsPage(){
-    this.newsService.getNewsPage(this.currentPage).subscribe(data => {this.newsPage = data})
-  }
+    getNewsPage() {
+        this.newsService.getNewsPage(this.currentPage).subscribe(data => {
+            this.newsPage = data;
+            this.collectionSize = data.CollectionSize;
+        })
+    }
 
 }
